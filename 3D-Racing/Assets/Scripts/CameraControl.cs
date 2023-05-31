28bb1cbc3e3430fd9f083e5fb7fa26891564d65d
shivadgss <46683373+shivadgss@users.noreply.github.com>
2023-05-31 14:59:48 +0530

    racetrack & car

---
+++ 3D-Racing/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject car;
+
+    public float carX;
+
+    public float carY;
+
+    public float carZ;
+    void Update()
+    {
+        carX = car.transform.eulerAngles.x;
+        carY = car.transform.eulerAngles.y;
+        carZ = car.transform.eulerAngles.z;
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+        
+    }
+}
